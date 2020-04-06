98549c45760a93927da14f3450ed33a77aedff3a
Cedric Mensah <cedric@mensah.de>
2020-04-06 18:08:57 +0200

    Added tracking of downloads

--- public/javascripts/downloads.js
+++ public/javascripts/downloads.js
@@ -1,6 +1,8 @@
 document.getElementById("germany").onclick = function () {
     document.getElementById("country").href = "test1"
     document.getElementById("country").innerText = "Download Germany"
+    appInsights.trackEvent({ name: "Download - Germany" });
+
 
 
 }
@@ -8,11 +10,13 @@ document.getElementById("germany").onclick = function () {
 document.getElementById("denmark").onclick = function () {
     document.getElementById("country").href = "test2"
     document.getElementById("country").textContent = "Download Denmark"
+    appInsights.trackEvent({ name: "Download - Denmark" });
 
 }
 
-
-
+document.getElementById("country").onclick = function () {
+    appInsights.trackEvent({ name: "Download - total" })
+}
 
 
 
