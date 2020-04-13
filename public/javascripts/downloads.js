a32f4807783fb947f13e54209f9ac5a0d01935e9
Cedric Mensah <cedric@mensah.de>
2020-04-13 21:57:06 +0200

    Final download links

--- public/javascripts/downloads.js
+++ public/javascripts/downloads.js
@@ -1,5 +1,5 @@
 document.getElementById("germany").onclick = function () {
-    document.getElementById("country").href = "test1"
+    document.getElementById("country").href = "https://facebookdata.blob.core.windows.net/thesisdata/germany_posts.csv"
     document.getElementById("country").innerText = "Download Germany"
     appInsights.trackEvent({ name: "Download - Germany" });
 
@@ -8,7 +8,7 @@ document.getElementById("germany").onclick = function () {
 }
 
 document.getElementById("denmark").onclick = function () {
-    document.getElementById("country").href = "test2"
+    document.getElementById("country").href = "https://facebookdata.blob.core.windows.net/thesisdata/denmark_posts.csv"
     document.getElementById("country").textContent = "Download Denmark"
     appInsights.trackEvent({ name: "Download - Denmark" });
 
