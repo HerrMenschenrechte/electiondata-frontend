b868c470e330884cf1ce23ca27c94c207bc75c4a
Cedric Mensah <cedric@mensah.de>
2020-05-03 22:30:49 +0200

    Updated tile post counter code

--- public/javascripts/SQLqueries.js
+++ public/javascripts/SQLqueries.js
@@ -1,5 +1,5 @@
 exports.getCountryCount = ''
 
-exports.getPostCount = 'SELECT(SELECT count(*) FROM denmark_posts) + (SELECT count(*) from germany_posts) + (SELECT count(*) from uk_posts) + (SELECT count(*) from france_posts) AS count'
+exports.getPostCount = 'SELECT(SELECT count(*) FROM denmark_posts) + (SELECT count(*) from germany_posts) + (SELECT count(*) from uk_posts) + (SELECT count(*) from france_posts) + (SELECT count(*) from hungary_posts) + (SELECT count(*) from slovakia_posts) AS count'
 
-exports.getLatestUpdate = 'SELECT(SELECT max(data_insert_timestamp)  FROM france_posts) AS date '
+exports.getLatestUpdate = 'SELECT(SELECT max(data_insert_timestamp)  FROM hungary_posts) AS date '
