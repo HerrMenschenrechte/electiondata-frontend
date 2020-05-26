4b719941d9a7650aa74b1ae2e4c90828f55b70b2
Cedric Mensah <cedric@mensah.de>
2020-05-26 13:48:32 +0200

    Fixed bug in post number count

--- public/javascripts/SQLqueries.js
+++ public/javascripts/SQLqueries.js
@@ -1,5 +1,5 @@
 exports.getCountryCount = ''
 
-exports.getPostCount = 'SELECT(SELECT count(*) FROM australia_posts) + SELECT(SELECT count(*) FROM denmark_posts) + (SELECT count(*) from germany_posts) + (SELECT count(*) from uk_posts) + (SELECT count(*) from france_posts) + (SELECT count(*) from hungary_posts) + (SELECT count(*) from slovakia_posts) AS count'
+exports.getPostCount = 'SELECT (SELECT count(*) FROM australia_posts) + (SELECT count(*) FROM denmark_posts) + (SELECT count(*) from germany_posts) + (SELECT count(*) from uk_posts) + (SELECT count(*) from france_posts) + (SELECT count(*) from hungary_posts) + (SELECT count(*) from slovakia_posts) AS count'
 
 exports.getLatestUpdate = 'SELECT(SELECT max(data_insert_timestamp)  FROM australia_posts) AS date '
