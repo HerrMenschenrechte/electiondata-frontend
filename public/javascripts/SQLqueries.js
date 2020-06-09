4591578ae81ab88c7c5f1656a3608ad370e1d2b6
Cedric Mensah <cedric@mensah.de>
2020-06-09 15:22:39 +0200

    Tiles show latest data update

--- public/javascripts/SQLqueries.js
+++ public/javascripts/SQLqueries.js
@@ -2,4 +2,4 @@ exports.getCountryCount = ''
 
 exports.getPostCount = 'SELECT (SELECT count(*) FROM india_posts) + (SELECT count(*) FROM australia_posts) + (SELECT count(*) FROM denmark_posts) + (SELECT count(*) from germany_posts) + (SELECT count(*) from uk_posts) + (SELECT count(*) from france_posts) + (SELECT count(*) from hungary_posts) + (SELECT count(*) from slovakia_posts) AS count'
 
-exports.getLatestUpdate = 'SELECT(SELECT max(data_insert_timestamp)  FROM australia_posts) AS date '
+exports.getLatestUpdate = 'SELECT(SELECT max(data_insert_timestamp)  FROM india_posts) AS date '
