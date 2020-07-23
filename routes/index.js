545202c7d77974c47b271963d35578590433746f
Cedric Mensah <cedric@mensah.de>
2020-07-23 11:45:47 +0200

    Deleted "async" keyword from routes

--- routes/index.js
+++ routes/index.js
@@ -3,42 +3,42 @@ const router = express.Router();
 
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Ad Transparency - Home' });
 
 });
 
-router.get('/blog', async function (req, res, next) {
+router.get('/blog', function (req, res) {
   res.render('blog', { title: 'Ad Transparency - Blog' });
 
 });
 
-router.get('/documentation', async function (req, res, next) {
+router.get('/documentation', function (req, res) {
   res.render('documentation', { title: 'Ad Transparency - Documentation' });
 
 });
 
-router.get('/about', async function (req, res, next) {
+router.get('/about', function (req, res) {
   res.render('about', { title: 'Ad Transparency - About' });
 
 });
 
-router.get('/contact', async function (req, res, next) {
+router.get('/contact', function (req, res) {
   res.render('contact', { title: 'Ad Transparency - Contact' });
 
 });
 
-router.get('/privacy', async function (req, res, next) {
+router.get('/privacy', function (req, res) {
   res.render('privacy', { title: 'Ad Transparency - Privacy' });
 
 });
 
-router.get('/legal', async function (req, res, next) {
+router.get('/legal', function (req, res) {
   res.render('legal', { title: 'Ad Transparency - Legal' });
 
 });
 
-router.get('/cookies', async function (req, res, next) {
+router.get('/cookies', function (req, res) {
   res.render('cookies', { title: 'Ad Transparency - Cookies' });
 
 });
