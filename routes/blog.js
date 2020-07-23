9fd5924d8d0ec775e27b5b33f310b6163c5afb00
Cedric Mensah <cedric@mensah.de>
2020-07-23 11:46:51 +0200

    deleted "async" keyword from blog routes

--- routes/blog.js
+++ routes/blog.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/article1', async function (req, res, next) {
+router.get('/article1', function (req, res) {
     res.render('article_1', { title: 'Ad Transparency - Hello World' });
 
 });
 
-router.get('/article2', async function (req, res, next) {
+router.get('/article2', function (req, res) {
     res.render('article_2', { title: 'Ad Transparency - How this page was built' });
 
 });
